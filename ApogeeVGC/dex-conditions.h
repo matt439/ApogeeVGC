97804c8619f94ebe77661889e02d8ee6b20471a1
matt439 <matt439@gmail.com>
2025-07-05 20:20:27 +1000

    add PokemonEventMethods' callbacks

--- ApogeeVGC/dex-conditions.h
+++ ApogeeVGC/dex-conditions.h
@@ -760,4 +760,176 @@ struct EventMethods {
 	std::optional<int> on_type_priority;
 };
 
-// PokemonEventMethods
+using OnAllyDamagingHitFunc = std::function<void(Battle*, int, Pokemon*, Pokemon*, ActiveMove*)>;
+using OnAllyAfterEachBoostFunc = std::function<void(Battle*, SparseBoostsTable, Pokemon*, Pokemon*)>;
+using OnAllyAfterHitFunc = OnAfterHitFunc; // from MoveEventMethods
+using OnAllyAfterSetStatusFunc = std::function<void(Battle*, Condition*, Pokemon*, Pokemon*, Effect)>;
+using OnAllyAfterSubDamageFunc = OnAfterSubDamageFunc; // from MoveEventMethods
+using OnAllyAfterSwitchInSelfFunc = std::function<void(Battle*, Pokemon*)>;
+using OnAllyAfterUseItemFunc = std::function<void(Battle*, Item*, Pokemon*)>;
+using OnAllyAfterBoostFunc = std::function<void(Battle*, SparseBoostsTable, Pokemon*, Pokemon*, Effect)>;
+using OnAllyAfterFaintFunc = std::function<void(Battle*, int, Pokemon*, Pokemon*, Effect)>;
+using OnAllyAfterMoveSecondarySelfFunc = OnAfterMoveSecondarySelfFunc; // from MoveEventMethods
+using OnAllyAfterMoveSecondaryFunc = OnAfterMoveSecondaryFunc; // from MoveEventMethods
+using OnAllyAfterMoveFunc = OnAfterMoveFunc; // from MoveEventMethods
+using OnAllyAfterMoveSelfFunc = VoidSourceMoveFunc; // from CommonHandlers
+using OnAllyAttractFunc = std::function<void(Battle*, Pokemon*, Pokemon*)>;
+using OnAllyAccuracyFunc = std::function<std::variant<int, bool, std::nullptr_t, void>(Battle*, int, Pokemon*, Pokemon*, ActiveMove*)>;
+
+using OnAllyBasePowerFunc = ModifierSourceMoveFunc; // from CommonHandlers
+using OnAllyBeforeFaintFunc = std::function<void(Battle*, Pokemon*, Effect)>;
+using OnAllyBeforeMoveFunc = VoidSourceMoveFunc; // from CommonHandlers
+using OnAllyBeforeSwitchInFunc = std::function<void(Battle*, Pokemon*)>;
+using OnAllyBeforeSwitchOutFunc = std::function<void(Battle*, Pokemon*)>;
+using OnAllyTryBoostFunc = std::function<void(Battle*, SparseBoostsTable, Pokemon*, Pokemon*, Effect)>;
+using OnAllyChargeMoveFunc = VoidSourceMoveFunc; // from CommonHandlers
+using OnAllyCriticalHitFunc = std::function<std::variant<bool, std::nullptr_t>(Battle*, Pokemon*, std::nullptr_t, ActiveMove*)>;
+using OnAllyDamageFunc = std::function<std::variant<int, bool, std::nullptr_t, void>(Battle*, int, Pokemon*, Pokemon*, Effect)>;
+using OnAllyDeductPPFunc = std::function<std::variant<int, void>(Battle*, Pokemon*, Pokemon*)>;
+using OnAllyDisableMoveFunc = std::function<void(Battle*, Pokemon*)>;
+using OnAllyDragOutFunc = std::function<void(Battle*, Pokemon*, std::optional<Pokemon*>, std::optional<ActiveMove*>)>;
+using OnAllyEatItemFunc = std::function<void(Battle*, Item*, Pokemon*)>;
+using OnAllyEffectivenessFunc = OnEffectivenessFunc; // from MoveEventMethods
+
+using OnAllyFaintFunc = VoidEffectFunc; // from CommonHandlers
+using OnAllyFlinchFunc = std::function<std::variant<bool, void>(Battle*, Pokemon*)>;
+using OnAllyHitFunc = OnHitFunc; // from MoveEventMethods
+using OnAllyImmunityFunc = std::function<void(Battle*, std::string, Pokemon*)>;
+using OnAllyLockMoveFunc = std::variant<std::string, std::function<std::variant<void, std::string>(Battle*, Pokemon*)>>;
+using OnAllyMaybeTrapPokemonFunc = std::function<void(Battle*, Pokemon*)>;
+using OnAllyModifyAccuracyFunc = ModifierMoveFunc; // from CommonHandlers
+using OnAllyModifyAtkFunc = ModifierSourceMoveFunc; // from CommonHandlers
+using OnAllyModifyBoostFunc = std::function<std::variant<SparseBoostsTable, void>(Battle*, SparseBoostsTable, Pokemon*)>;
+using OnAllyModifyCritRatioFunc = ModifierSourceMoveFunc; // from CommonHandlers
+using OnAllyModifyDamageFunc = ModifierSourceMoveFunc; // from CommonHandlers
+using OnAllyModifyDefFunc = ModifierMoveFunc; // from CommonHandlers
+using OnAllyModifyMoveFunc = OnModifyMoveFunc; // from MoveEventMethods
+using OnAllyModifyPriorityFunc = ModifierSourceMoveFunc; // from CommonHandlers
+using OnAllyModifySecondariesFunc = std::function<void(Battle*, std::vector<SecondaryEffect>&, Pokemon*, Pokemon*, ActiveMove*)>;
+
+using OnAllyModifySpAFunc = ModifierSourceMoveFunc; // from CommonHandlers
+using OnAllyModifySpDFunc = ModifierMoveFunc; // from CommonHandlers
+using OnAllyModifySpeFunc = std::function<std::variant<int, void>(Battle*, int, Pokemon*)>;
+using OnAllyModifySTABFunc = ModifierSourceMoveFunc; // from CommonHandlers
+using OnAllyModifyTypeFunc = OnModifyTypeFunc; // from MoveEventMethods
+using OnAllyModifyTargetFunc = OnModifyTargetFunc; // from MoveEventMethods
+using OnAllyModifyWeightFunc = std::function<std::variant<int, void>(Battle*, int, Pokemon*)>;
+using OnAllyMoveAbortedFunc = VoidMoveFunc; // from CommonHandlers
+using OnAllyNegateImmunityFunc = std::variant<std::function<std::variant<bool, void>(Battle*, Pokemon*, std::string)>, bool>;
+using OnAllyOverrideActionFunc = std::function<std::variant<std::string, void>(Battle*, Pokemon*, Pokemon*, ActiveMove*)>;
+using OnAllyPrepareHitFunc = ResultSourceMoveFunc; // from CommonHandlers
+using OnAllyRedirectTargetFunc = std::function<std::variant<Pokemon*, void>(Battle*, Pokemon*, Pokemon*, Effect, ActiveMove*)>;
+using OnAllyResidualFunc = std::function<void(Battle*, Pokemon&, Pokemon*, Effect)>;
+using OnAllySetAbilityFunc = std::function<std::variant<bool, void>(Battle*, std::string, Pokemon*, Pokemon*, Effect)>;
+
+using OnAllySetStatusFunc = std::function<std::variant<bool, std::nullptr_t, void>(Battle*, Condition*, Pokemon*, Pokemon*, Effect)>;
+using OnAllySetWeatherFunc = std::function<std::variant<bool, void>(Battle*, Pokemon*, Pokemon*, Condition*)>;
+using OnAllyStallMoveFunc = std::function<std::variant<bool, void>(Battle*, Pokemon*)>;
+using OnAllySwitchOutFunc = std::function<void(Battle*, Pokemon*)>;
+using OnAllyTakeItemFunc = std::variant<std::function<std::variant<bool, void>(Battle*, Item*, Pokemon*, Pokemon*, std::optional<ActiveMove>)>, bool>;
+using OnAllyTerrainFunc = std::function<void(Battle*, Pokemon*)>;
+using OnAllyTrapPokemonFunc = std::function<void(Battle*, Pokemon*)>;
+using OnAllyTryAddVolatileFunc = std::function<std::variant<bool, std::nullptr_t, void>(Battle*, Condition*, Pokemon*, Pokemon*, Effect)>;
+using OnAllyTryEatItemFunc = std::variant<bool, std::function<std::variant<bool, void>(Battle*, Item*, Pokemon*)>>;
+using OnAllyTryHealFunc = std::variant<
+	std::function<std::variant<int, bool, void>(Battle*, int, Pokemon*, Pokemon*, Effect)>,
+	std::function<std::variant<bool, void>(Battle*, Pokemon*)>,
+	bool>;
+
+using OnAllyTryHitFunc = OnTryHitFunc; // from MoveEventMethods
+using OnAllyTryHitFieldFunc = OnTryHitFieldFunc; // from MoveEventMethods
+using OnAllyTryHitSideFunc = ResultMoveFunc; // from CommonHandlers
+using OnAllyInvulnerabilityFunc = ExtResultMoveFunc; // from CommonHandlers
+using OnAllyTryMoveFunc = OnTryMoveFunc; // from MoveEventMethods
+using OnAllyTryPrimaryHitFunc = std::function<std::variant<bool, std::nullptr_t, int, void>(Battle*, Pokemon*, Pokemon*, ActiveMove*)>;
+using OnAllyTypeFunc = OnTypeFunc; // from MoveEventMethods
+using OnAllyWeatherModifyDamageFunc = ModifierSourceMoveFunc; // from CommonHandlers
+using OnAllyModifyDamagePhase1Func = ModifierSourceMoveFunc; // from CommonHandlers
+using OnAllyModifyDamagePhase2Func = ModifierSourceMoveFunc; // from CommonHandlers
+
+struct PokemonEventMethods : public EventMethods
+{
+	std::optional<OnAllyDamagingHitFunc> on_ally_damaging_hit;
+	std::optional<OnAllyAfterEachBoostFunc> on_ally_after_each_boost;	
+	std::optional<OnAllyAfterHitFunc> on_ally_after_hit;
+	std::optional<OnAllyAfterSetStatusFunc> on_ally_after_set_status;
+	std::optional<OnAllyAfterSubDamageFunc> on_ally_after_sub_damage;
+	std::optional<OnAllyAfterSwitchInSelfFunc> on_ally_after_switch_in_self;
+	std::optional<OnAllyAfterUseItemFunc> on_ally_after_use_item;
+	std::optional<OnAllyAfterBoostFunc> on_ally_after_boost;
+	std::optional<OnAllyAfterFaintFunc> on_ally_after_faint;
+	std::optional<OnAllyAfterMoveSecondarySelfFunc> on_ally_after_move_secondary_self;
+	std::optional<OnAllyAfterMoveSecondaryFunc> on_ally_after_move_secondary;
+	std::optional<OnAllyAfterMoveFunc> on_ally_after_move;
+	std::optional<OnAllyAfterMoveSelfFunc> on_ally_after_move_self;
+	std::optional<OnAllyAttractFunc> on_ally_attract;
+	std::optional<OnAllyAccuracyFunc> on_ally_accuracy;
+
+	std::optional<OnAllyBasePowerFunc> on_ally_base_power;
+	std::optional<OnAllyBeforeFaintFunc> on_ally_before_faint;
+	std::optional<OnAllyBeforeMoveFunc> on_ally_before_move;
+	std::optional<OnAllyBeforeSwitchInFunc> on_ally_before_switch_in;
+	std::optional<OnAllyBeforeSwitchOutFunc> on_ally_before_switch_out;
+	std::optional<OnAllyTryBoostFunc> on_ally_try_boost;
+	std::optional<OnAllyChargeMoveFunc> on_ally_charge_move;
+	std::optional<OnAllyCriticalHitFunc> on_ally_critical_hit;
+	std::optional<OnAllyDamageFunc> on_ally_damage;
+	std::optional<OnAllyDeductPPFunc> on_ally_deduct_pp;
+	std::optional<OnAllyDisableMoveFunc> on_ally_disable_move;
+	std::optional<OnAllyDragOutFunc> on_ally_drag_out;
+	std::optional<OnAllyEatItemFunc> on_ally_eat_item;
+	std::optional<OnAllyEffectivenessFunc> on_ally_effectiveness;
+
+	std::optional<OnAllyFaintFunc> on_ally_faint;
+	std::optional<OnAllyFlinchFunc> on_ally_flinch;
+	std::optional<OnAllyHitFunc> on_ally_hit;
+	std::optional<OnAllyImmunityFunc> on_ally_immunity;
+	std::optional<OnAllyLockMoveFunc> on_ally_lock_move;
+	std::optional<OnAllyMaybeTrapPokemonFunc> on_ally_maybe_trap_pokemon;
+	std::optional<OnAllyModifyAccuracyFunc> on_ally_modify_accuracy;
+	std::optional<OnAllyModifyAtkFunc> on_ally_modify_atk;
+	std::optional<OnAllyModifyBoostFunc> on_ally_modify_boost;
+	std::optional<OnAllyModifyCritRatioFunc> on_ally_modify_crit_ratio;
+	std::optional<OnAllyModifyDamageFunc> on_ally_modify_damage;
+	std::optional<OnAllyModifyDefFunc> on_ally_modify_def;
+	std::optional<OnAllyModifyMoveFunc> on_ally_modify_move;
+	std::optional<OnAllyModifyPriorityFunc> on_ally_modify_priority;
+	std::optional<OnAllyModifySecondariesFunc> on_ally_modify_secondaries;
+
+	std::optional<OnAllyModifySpAFunc> on_ally_modify_spa;
+	std::optional<OnAllyModifySpDFunc> on_ally_modify_spd;
+	std::optional<OnAllyModifySpeFunc> on_ally_modify_spe;
+	std::optional<OnAllyModifySTABFunc> on_ally_modify_stab;
+	std::optional<OnAllyModifyTypeFunc> on_ally_modify_type;
+	std::optional<OnAllyModifyTargetFunc> on_ally_modify_target;
+	std::optional<OnAllyModifyWeightFunc> on_ally_modify_weight;
+	std::optional<OnAllyMoveAbortedFunc> on_ally_move_aborted;
+	std::optional<OnAllyNegateImmunityFunc> on_ally_negate_immunity;
+	std::optional<OnAllyOverrideActionFunc> on_ally_override_action;
+	std::optional<OnAllyPrepareHitFunc> on_ally_prepare_hit;
+	std::optional<OnAllyRedirectTargetFunc> on_ally_redirect_target;
+	std::optional<OnAllyResidualFunc> on_ally_residual;
+	std::optional<OnAllySetAbilityFunc> on_ally_set_ability;
+
+	std::optional<OnAllySetStatusFunc> on_ally_set_status;
+	std::optional<OnAllySetWeatherFunc> on_ally_set_weather;
+	std::optional<OnAllyStallMoveFunc> on_ally_stall_move;
+	std::optional<OnAllySwitchOutFunc> on_ally_switch_out;
+	std::optional<OnAllyTakeItemFunc> on_ally_take_item;
+	std::optional<OnAllyTerrainFunc> on_ally_terrain_change;
+	std::optional<OnAllyTrapPokemonFunc> on_ally_trap_pokemon;
+	std::optional<OnAllyTryAddVolatileFunc> on_ally_try_add_volatile;
+	std::optional<OnAllyTryEatItemFunc> on_ally_try_eat_item;
+	std::optional<OnAllyTryHealFunc> on_ally_try_heal;
+
+	std::optional<OnAllyTryHitFunc> on_ally_try_hit;
+	std::optional<OnAllyTryHitFieldFunc> on_ally_try_hit_field;
+	std::optional<OnAllyTryHitSideFunc> on_ally_try_hit_side;
+	std::optional<OnAllyInvulnerabilityFunc> on_ally_invulnerability;
+	std::optional<OnAllyTryMoveFunc> on_ally_try_move;
+	std::optional<OnAllyTryPrimaryHitFunc> on_ally_try_primary_hit;
+	std::optional<OnAllyTypeFunc> on_ally_type;
+	std::optional<OnAllyWeatherModifyDamageFunc> on_ally_weather_modify_damage;
+	std::optional<OnAllyModifyDamagePhase1Func> on_ally_modify_damage_phase_1;
+	std::optional<OnAllyModifyDamagePhase2Func> on_ally_modify_damage_phase_2;
+};
