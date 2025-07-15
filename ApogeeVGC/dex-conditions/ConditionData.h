4cfe17f3deeabd2381e9cfa0e219f414f3d29c1c
matt439 <matt439@gmail.com>
2025-07-15 19:01:06 +1000

    ConditionData is now fully implemented and still compiles

--- ApogeeVGC/dex-conditions/ConditionData.h
+++ ApogeeVGC/dex-conditions/ConditionData.h
@@ -1,19 +1,8 @@
 #pragma once
 
 #include "PokemonConditionData.h"
-//#include "SideConditionData.h"
-//#include "FieldConditionData.h"
-//#include <variant>
+#include "SideConditionData.h"
+#include "FieldConditionData.h"
+#include <variant>
 
-//using ConditionData = std::variant<PokemonConditionData, SideConditionData, FieldConditionData>;
-
-struct ConditionData
-{
-	std::unique_ptr<PokemonConditionData> pokemon_condition = nullptr; // optional
-	//std::unique_ptr<SideConditionData> side_condition = nullptr; // optional
-	//std::unique_ptr<FieldConditionData> field_condition = nullptr; // optional
-	// int x = 0;
-
-	ConditionData() = default;
-	ConditionData(const ConditionData& other);
-};
+using ConditionData = std::variant<PokemonConditionData, SideConditionData, FieldConditionData>;
