e771bd033a6cab6556fabb01a475e74e9c62ba42
matt439 <matt439@gmail.com>
2025-07-31 12:49:57 +1000

    add to state

--- ApogeeVGC_CS/sim/state.cs
+++ ApogeeVGC_CS/sim/state.cs
@@ -11,6 +11,42 @@ namespace ApogeeVGC_CS.sim
         // Types that are serialized as references
         public static readonly HashSet<Type> ReferableTypes = new()
         {
+        public static readonly HashSet<string> BATTLE = new()
+        {
+            "Dex", "Gen", "RuleTable", "Id", "Log", "inherit", "Format", "TeamGenerator",
+            "HitSubstitute", "NotFail", "Fail", "SilentFail", "Field", "Sides", "Prng", "Hints",
+            "Deserialized", "queue", "Actions"
+        };
+
+        public static readonly HashSet<string> FIELD = new() { "Id", "Battle" };
+
+        public static readonly HashSet<string> SIDE = new() { "Battle", "team", "Pokemon", "Choice", "ActiveRequest" };
+
+        public static readonly HashSet<string> POKEMON = new()
+        {
+            "Side", "Battle", "set", "name", "fullname", "id",
+            "happiness", "level", "pokeball", "baseMoveSlots"
+        };
+
+        public static readonly HashSet<string> CHOICE = new() { "SwitchIns" };
+
+        public static readonly HashSet<string> ACTIVE_MOVE = new() { "move" };
+    }
+
+    public class State
+    {
+        // REFERABLE is used to determine which objects are of the Referable type by
+        // comparing their constructors. We set this dynamically due to circular module dependencies.
+        private HashSet<Type>? _referable;
+
+        private HashSet<Type> REFERABLE
+        {
+            get
+            {
+                if (_referable == null)
+                {
+                    _referable = new HashSet<Type>
+                    {
             typeof(Battle), typeof(Field), typeof(Side), typeof(Pokemon),
             typeof(Condition), typeof(Ability), typeof(Item), typeof(Move), typeof(Species)
         };
