a1954c167ef7d9bc4b21fe44bbb21e26318e38d4
matt439 <matt439@gmail.com>
2025-09-04 20:25:03 +1000

    organise effects

---
+++ ApogeeVGC/Sim/Effects/EffectType.cs
@@ -0,0 +1,11 @@
+﻿namespace ApogeeVGC.Sim.Effects;
+
+public enum EffectType
+{
+    Ability,
+    Item,
+    Move,
+    Specie,
+    Condition,
+    Format,
+}
---
+++ ApogeeVGC/Sim/Utils/Extensions/EffectExtensions.cs
@@ -0,0 +1,22 @@
+﻿using ApogeeVGC.Sim.Effects;
+
+namespace ApogeeVGC.Sim.Utils.Extensions;
+
+public static class EffectExtensions
+{
+    /// <summary>
+    /// Determines if this effect is of a specific type
+    /// </summary>
+    public static bool IsOfType(this IEffect effect, EffectType effectType)
+    {
+        return effect.EffectType == effectType;
+    }
+
+    /// <summary>
+    /// Safely casts an IEffect to a specific type if possible
+    /// </summary>
+    public static T? As<T>(this IEffect effect) where T : class, IEffect
+    {
+        return effect as T;
+    }
+}
