ed6e35a07673173e46d4a0831ab04c0baedc0c08
matt439 <matt439@gmail.com>
2025-10-25 14:55:03 +1000

    add BattleAsync.Delegates.cs

--- ApogeeVGC/Sim/BattleClasses/BattleAsync.Core.cs
+++ ApogeeVGC/Sim/BattleClasses/BattleAsync.Core.cs
@@ -1,6 +1,4 @@
 ﻿using ApogeeVGC.Data;
-using ApogeeVGC.Sim.Actions;
-using ApogeeVGC.Sim.Choices;
 using ApogeeVGC.Sim.Core;
 using ApogeeVGC.Sim.Effects;
 using ApogeeVGC.Sim.Events;
@@ -9,10 +7,7 @@ using ApogeeVGC.Sim.GameObjects;
 using ApogeeVGC.Sim.Moves;
 using ApogeeVGC.Sim.PokemonClasses;
 using ApogeeVGC.Sim.SideClasses;
-using ApogeeVGC.Sim.Stats;
 using ApogeeVGC.Sim.Utils;
-using ApogeeVGC.Sim.Utils.Extensions;
-using static ApogeeVGC.Sim.PokemonClasses.Pokemon;
 
 namespace ApogeeVGC.Sim.BattleClasses;
 
@@ -204,55 +199,10 @@ public partial class BattleAsync : IBattle, IDisposable
         Prng = new Prng(seed);
     }
 
-    
-
-    
-
-    
-
-    
-
-    
-
-    
-
-    
-
-    
-
-    
-
-    
-
-    
-
-    
-
-    
-
-    
-
-    
-
-    
-
-    
-
-    
-
     public static IReadOnlyList<PokemonSet> GetTeam(PlayerOptions options)
     {
         return options.Team ?? throw new InvalidOperationException();
     }
-
-    
-
-    
-
-    
-
-    
-    
 
     /// <summary>
     /// Initializes an EffectState object with proper effect ordering.
@@ -383,188 +333,4 @@ public partial class BattleAsync : IBattle, IDisposable
     {
         throw new NotImplementedException();
     }
-
-    
-
-    #region Helpers
-
-    
-
-
-    /// <summary>
-    /// Invokes a DelegateEffectDelegate by attempting common delegate signatures.
-    /// Falls back to reflection only when necessary.
-    /// Optimized to minimize allocations by reusing a fixed-size array.
-    /// </summary>
-    private RelayVar? InvokeDelegateEffectDelegate(Delegate del, bool hasRelayVar, RelayVar relayVar,
-        SingleEventTarget? target, SingleEventSource? source, IEffect? sourceEffect)
-    {
-        // Cache parameter info to avoid repeated reflection calls
-        var parameters = del.Method.GetParameters();
-        int paramCount = parameters.Length;
-
-        // Most common signature: (IBattle battle, ...)
-        if (paramCount == 0)
-        {
-            return (RelayVar?)del.DynamicInvoke(null);
-        }
-
-        // Optimize for the most common cases (1-5 parameters)
-        // This avoids array allocation for the majority of callbacks
-        object? result;
-        switch (paramCount)
-        {
-            case 1:
-                result = del.DynamicInvoke(BuildSingleArg(parameters[0], hasRelayVar, relayVar, target, source, sourceEffect));
-                return (RelayVar?)result;
-            case 2:
-                result = del.DynamicInvoke(
-                    BuildSingleArg(parameters[0], hasRelayVar, relayVar, target, source, sourceEffect),
-                    BuildSingleArg(parameters[1], hasRelayVar, relayVar, target, source, sourceEffect, 1)
-                );
-                return (RelayVar?)result;
-            case 3:
-                result = del.DynamicInvoke(
-                    BuildSingleArg(parameters[0], hasRelayVar, relayVar, target, source, sourceEffect),
-                    BuildSingleArg(parameters[1], hasRelayVar, relayVar, target, source, sourceEffect, 1),
-                    BuildSingleArg(parameters[2], hasRelayVar, relayVar, target, source, sourceEffect, 2)
-                );
-                return (RelayVar?)result;
-            case 4:
-                result = del.DynamicInvoke(
-                    BuildSingleArg(parameters[0], hasRelayVar, relayVar, target, source, sourceEffect),
-                    BuildSingleArg(parameters[1], hasRelayVar, relayVar, target, source, sourceEffect, 1),
-                    BuildSingleArg(parameters[2], hasRelayVar, relayVar, target, source, sourceEffect, 2),
-                    BuildSingleArg(parameters[3], hasRelayVar, relayVar, target, source, sourceEffect, 3)
-                );
-                return (RelayVar?)result;
-        }
-
-        // Fallback for 5+ parameters (rare)
-        // Use array allocation for these cases
-        object?[] args = new object?[paramCount];
-        int argIndex = 0;
-
-        // First parameter is typically IBattle (this)
-        if (parameters[0].ParameterType.IsAssignableFrom(typeof(IBattle)))
-        {
-            args[argIndex++] = this;
-        }
-
-        // Add relayVar if it was explicitly provided and if the delegate expects it
-        if (hasRelayVar)
-        {
-            args[argIndex++] = relayVar;
-        }
-
-        // Add remaining standard parameters: target, source, sourceEffect
-        while (argIndex < paramCount)
-        {
-            Type paramType = parameters[argIndex].ParameterType;
-
-            // Try to match target parameter
-            if (target != null)
-            {
-                EventTargetParameter? targetParam = EventTargetParameter.FromSingleEventTarget(target, paramType);
-                if (targetParam != null)
-                {
-                    args[argIndex++] = targetParam.ToObject();
-                    continue;
-                }
-            }
-
-            // Try to match source parameter
-            if (source != null)
-            {
-                EventSourceParameter? sourceParam = EventSourceParameter.FromSingleEventSource(source, paramType);
-                if (sourceParam != null)
-                {
-                    args[argIndex++] = sourceParam.ToObject();
-                    continue;
-                }
-            }
-
-            // Try to match sourceEffect parameter
-            if (sourceEffect != null && paramType.IsInstanceOfType(sourceEffect))
-            {
-                args[argIndex++] = sourceEffect;
-                continue;
-            }
-
-            // If we couldn't match, add null
-            args[argIndex++] = null;
-        }
-
-        result = del.DynamicInvoke(args);
-        return (RelayVar?)result;
-    }
-
-    /// <summary>
-    /// Builds a single argument for delegate invocation.
-    /// Used by the optimized fast-path for common parameter counts.
-    /// </summary>
-    private object? BuildSingleArg(System.Reflection.ParameterInfo param, bool hasRelayVar, RelayVar relayVar,
-        SingleEventTarget? target, SingleEventSource? source, IEffect? sourceEffect, int position = 0)
-    {
-        Type paramType = param.ParameterType;
-
-        // First parameter is typically IBattle
-        if (position == 0 && paramType.IsAssignableFrom(typeof(IBattle)))
-        {
-            return this;
-        }
-
-        // Second parameter might be relayVar if explicitly provided
-        if (position == 1 && hasRelayVar)
-        {
-            return relayVar;
-        }
-
-        // Adjust position if IBattle was first
-        int adjustedPos = position;
-        if (position > 0 && paramType.IsAssignableFrom(typeof(IBattle)))
-        {
-            adjustedPos--;
-        }
-        if (hasRelayVar && adjustedPos > 0)
-        {
-            adjustedPos--;
-        }
-
-        // Try to match standard parameters in order: target, source, sourceEffect
-        switch (adjustedPos)
-        {
-            case 0:
-                // Try target first
-                if (target != null)
-                {
-                    EventTargetParameter? targetParam = EventTargetParameter.FromSingleEventTarget(target, paramType);
-                    if (targetParam != null) return targetParam.ToObject();
-                }
-                break;
-            case 1:
-                // Try source second
-                if (source != null)
-                {
-                    EventSourceParameter? sourceParam = EventSourceParameter.FromSingleEventSource(source, paramType);
-                    if (sourceParam != null) return sourceParam.ToObject();
-                }
-                break;
-            case 2:
-                // Try sourceEffect third
-                if (sourceEffect != null && paramType.IsInstanceOfType(sourceEffect))
-                {
-                    return sourceEffect;
-                }
-                break;
-        }
-
-        return null;
-    }
-
-    
-
-    
-
-    #endregion
 }
