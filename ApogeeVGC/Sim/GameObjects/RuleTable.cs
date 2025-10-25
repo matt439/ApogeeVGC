438a68604cdfb4326e00b88bc19f5077a25f4874
matt439 <matt439@gmail.com>
2025-10-25 14:15:23 +1000

    bug fix to rule table

--- ApogeeVGC/Sim/GameObjects/RuleTable.cs
+++ ApogeeVGC/Sim/GameObjects/RuleTable.cs
@@ -8,11 +8,22 @@ public class RuleTable : Dictionary<RuleId, Rule>
 
     public bool Has(RuleId rule)
     {
-        throw new NotImplementedException();
+        return ContainsKey(rule);
     }
 
     public bool Has(AbilityId ability)
     {
-        throw new NotImplementedException();
+        // Check if there's a rule that corresponds to banning this ability
+        // In Pokemon Showdown, banned abilities are stored as rules with the same name
+        // We need to convert the AbilityId to a RuleId by finding a rule with the same name
+        // For now, we'll check if a RuleId exists with the ability's name
+
+        // Try to parse the ability ID as a rule ID
+        if (Enum.TryParse(ability.ToString(), out RuleId ruleId))
+        {
+            return ContainsKey(ruleId);
+        }
+
+        return false;
     }
 }
