d40b3028d32f357d692002c0a70293a4fd4018af
matt439 <matt439@gmail.com>
2025-11-01 16:48:53 +1000

    added more EventIdInfo

--- ApogeeVGC/Data/EventIdInfoData.cs
+++ ApogeeVGC/Data/EventIdInfoData.cs
@@ -43,6 +43,148 @@ public record EventIdInfoData
             UsesSpeed = true,
             UsesFractionalSpeed = true,
         },
-        // TODO: Fill in the rest of the event infos
+        [EventId.Residual] = new EventIdInfo
+        {
+            Id = EventId.Residual,
+            UsesSpeed = true,
+        },
+        [EventId.FieldResidual] = new EventIdInfo
+        {
+            Id = EventId.FieldResidual,
+            FieldEventId = EventId.FieldResidual,
+            BaseEventId = EventId.Residual,
+            UsesSpeed = true,
+        },
+        [EventId.SideResidual] = new EventIdInfo
+        {
+            Id = EventId.SideResidual,
+            SideEventId = EventId.SideResidual,
+            BaseEventId = EventId.Residual,
+            UsesSpeed = true,
+        },
+        [EventId.BeforeTurn] = new EventIdInfo
+        {
+            Id = EventId.BeforeTurn,
+            UsesSpeed = true,
+        },
+        [EventId.Update] = new EventIdInfo
+        {
+            Id = EventId.Update,
+            UsesSpeed = true,
+        },
+        [EventId.EmergencyExit] = new EventIdInfo
+        {
+            Id = EventId.EmergencyExit,
+            UsesSpeed = true,
+        },
+        [EventId.DisableMove] = new EventIdInfo
+        {
+            Id = EventId.DisableMove,
+            UsesSpeed = true,
+        },
+        [EventId.TrapPokemon] = new EventIdInfo
+        {
+            Id = EventId.TrapPokemon,
+            UsesSpeed = true,
+        },
+        [EventId.MaybeTrapPokemon] = new EventIdInfo
+        {
+            Id = EventId.MaybeTrapPokemon,
+            UsesSpeed = true,
+        },
+        [EventId.FoeMaybeTrapPokemon] = new EventIdInfo
+        {
+            Id = EventId.FoeMaybeTrapPokemon,
+            Prefix = EventPrefix.Foe,
+            BaseEventId = EventId.MaybeTrapPokemon,
+            UsesSpeed = true,
+        },
+        [EventId.BeforeSwitchOut] = new EventIdInfo
+        {
+            Id = EventId.BeforeSwitchOut,
+            UsesSpeed = true,
+        },
+        [EventId.ModifyPriority] = new EventIdInfo
+        {
+            Id = EventId.ModifyPriority,
+            UsesSpeed = true,
+        },
+        [EventId.Start] = new EventIdInfo
+        {
+            Id = EventId.Start,
+            UsesSpeed = true,
+        },
+        [EventId.CheckShow] = new EventIdInfo
+        {
+            Id = EventId.CheckShow,
+            UsesSpeed = true,
+        },
+        [EventId.BasePower] = new EventIdInfo
+        {
+            Id = EventId.BasePower,
+            UsesSpeed = true,
+        },
+        [EventId.ModifyAtk] = new EventIdInfo
+        {
+            Id = EventId.ModifyAtk,
+            UsesSpeed = true,
+        },
+        [EventId.ModifyDef] = new EventIdInfo
+        {
+            Id = EventId.ModifyDef,
+            UsesSpeed = true,
+        },
+        [EventId.ModifySpA] = new EventIdInfo
+        {
+            Id = EventId.ModifySpA,
+            UsesSpeed = true,
+        },
+        [EventId.ModifySpD] = new EventIdInfo
+        {
+            Id = EventId.ModifySpD,
+            UsesSpeed = true,
+        },
+        [EventId.ModifySpe] = new EventIdInfo
+        {
+            Id = EventId.ModifySpe,
+            UsesSpeed = true,
+        },
+        [EventId.TryHit] = new EventIdInfo
+        {
+            Id = EventId.TryHit,
+            UsesSpeed = true,
+            UsesLeftToRightOrder = true,
+        },
+        [EventId.Invulnerability] = new EventIdInfo
+        {
+            Id = EventId.Invulnerability,
+            UsesSpeed = true,
+            UsesLeftToRightOrder = true,
+        },
+        [EventId.DamagingHit] = new EventIdInfo
+        {
+            Id = EventId.DamagingHit,
+            UsesSpeed = true,
+            UsesLeftToRightOrder = true,
+        },
+        [EventId.EntryHazard] = new EventIdInfo
+        {
+            Id = EventId.EntryHazard,
+            UsesSpeed = true,
+            UsesLeftToRightOrder = true,
+        },
+        [EventId.AllyTryHitSide] = new EventIdInfo
+        {
+            Id = EventId.AllyTryHitSide,
+            Prefix = EventPrefix.Ally,
+            BaseEventId = EventId.TryHitSide,
+            UsesSpeed = true,
+        },
+        [EventId.TryHitSide] = new EventIdInfo
+        {
+            Id = EventId.TryHitSide,
+            UsesSpeed = true,
+        },
+        // Add more events as needed
     };
 }
